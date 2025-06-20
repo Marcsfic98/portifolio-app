6f29aa9818e5c16bf8a676f7206403fe990aec12
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-06-20 10:18:08 -0300

    atualizaçao

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import {  View } from 'react-native';
 import { Home } from './src/screen/Home';
 import { Photo } from './src/components/Photo';
 import { Skill } from './src/screen/Skill';
+import { MyTabs } from './src/routes/ButtonTab';
 
 export default function App() {
   return (
     <View >
-      <
+      <MyTabs/>
     </View>
   );
 }
