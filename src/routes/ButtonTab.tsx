1ee795d034e6a456b4226352baf55d820b514230
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-06-20 22:04:00 -0300

    projeto finalizado

--- src/routes/ButtonTab.tsx
+++ src/routes/ButtonTab.tsx
@@ -1,14 +1,32 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "../screen/Home";
 import { Skill } from "../screen/Skill";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const {Navigator,Screen} = createBottomTabNavigator()
+const { Navigator, Screen } = createBottomTabNavigator();
 
 export function MyTabs() {
-    return(
-        <Navigator screenOptions={{headerShown:false, tabBarStyle:{backgroundColor:'#8130a1',borderTopWidth:0,elevation:0,shadowOpacity:0}}}>
-            <Screen name="Home" component={Home}/>
-            <Screen name="Skills" component={Skill}/>
-        </Navigator>
-    )
+  return (
+    <Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: { backgroundColor: '#8130a1', borderTopWidth: 0 },
+        tabBarActiveTintColor: "black",
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Skills') {
+            iconName = focused ? 'book' : 'book-outline';
+          }
+
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Screen name="Home" component={Home} />
+      <Screen name="Skills" component={Skill} />
+    </Navigator>
+  );
 }
